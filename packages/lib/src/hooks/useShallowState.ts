ff5357ec2907d782540dea1fa8bb161d8a8f6f5d
minjaeleee <your-email@example.com>
2025-07-21 23:16:29 +0900

    feat: useShallowState 기능 구현 1. shallowEquals로 비교 2. useCallback으로 setState를 고정하여 항상 같은 참조를 유지하게 함 3. resolveNextState 함수형 업데이트 처리

--- packages/lib/src/hooks/useShallowState.ts
+++ packages/lib/src/hooks/useShallowState.ts
@@ -1,8 +1,22 @@
-import { useState } from "react";
-// import { shallowEquals } from "../equals";
+import { useState, useCallback } from "react";
+import type { Dispatch, SetStateAction } from "react";
+import { shallowEquals } from "../equals";
 
-// export const useShallowState = <T>(initialValue: Parameters<typeof useState<T>>[0]) => {
-export const useShallowState = <T>(initialValue: Parameters<typeof useState<T>>) => {
-  // useState를 사용하여 상태를 관리하고, shallowEquals를 사용하여 상태 변경을 감지하는 훅을 구현합니다.
-  return useState(initialValue);
+// shallowEquals를 사용하여 상태 변경을 감지하는 훅을 구현합니다.
+function resolveNextState<T>(prevState: T, newValue: SetStateAction<T>): T {
+  // 함수형 업데이트인 경우 예외처리
+  const nextState = typeof newValue === "function" ? (newValue as (prevState: T) => T)(prevState) : newValue;
+
+  return shallowEquals(prevState, nextState) ? prevState : nextState;
+}
+
+export const useShallowState = <T>(initialValue: T | (() => T)): [T, Dispatch<SetStateAction<T>>] => {
+  // useState를 사용하여 상태를 관리하고,
+  const [state, setState] = useState<T>(initialValue);
+
+  const setShallowState = useCallback<Dispatch<SetStateAction<T>>>((newValue) => {
+    setState((prevState) => resolveNextState(prevState, newValue));
+  }, []);
+
+  return [state, setShallowState];
 };
