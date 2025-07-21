e4487495e7e2075e2e2133d8ce0de6dbfdfd5ca8
minjaeleee <your-email@example.com>
2025-07-21 23:47:31 +0900

    feat: useAutoCallback 기능 구현 1. 콜백함수가 참조하는 값을 렌더링 시점에 최신화 시키는 기능 2. useCallback으로 항상 동일한 참조를 유지하게 시키는 기능

--- packages/lib/src/hooks/useAutoCallback.ts
+++ packages/lib/src/hooks/useAutoCallback.ts
@@ -1,7 +1,17 @@
 import type { AnyFunction } from "../types";
-// import { useCallback } from "./useCallback";
-// import { useRef } from "./useRef";
+import { useCallback } from "./useCallback";
+import { useRef } from "./useRef";
 
+// useCallback과 useRef를 이용하여 useAutoCallback
 export const useAutoCallback = <T extends AnyFunction>(fn: T): T => {
-  return fn;
+  // 1. 콜백함수가 참조하는 값은 항상 렌더링 시점에 최신화 되어야 한다.
+  const callbackFnRef = useRef(fn);
+  callbackFnRef.current = fn;
+
+  // 2. 대신 항상 동일한 참조를 유지해야 한다. (useCallback 활용)
+  const stableFn = useCallback((...args: unknown[]) => {
+    return callbackFnRef.current(...args);
+  }, []);
+
+  return stableFn as T;
 };
