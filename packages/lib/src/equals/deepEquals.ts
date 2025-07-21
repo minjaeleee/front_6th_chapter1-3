a981057732810d4a1eb6b20d5a3ff36a6a65c2d3
minjaeleee <your-email@example.com>
2025-07-22 00:25:29 +0900

    fix: deepEqauls 함수 기능 수정 1. 순수 객체 비교시, a 객체의 key만 비교하는 로직이므로 깊은 비교를 수행할 때, 정상적인 기능을 하지 못하는 이슈 수정

--- packages/lib/src/equals/deepEquals.ts
+++ packages/lib/src/equals/deepEquals.ts
@@ -5,19 +5,21 @@ export const deepEquals = (a: unknown, b: unknown): boolean => {
 
   // 배열인 경우 재귀 비교
   if (Array.isArray(a) && Array.isArray(b)) {
+    if (a.length !== b.length) return false;
     for (let i = 0; i < a.length; i++) {
       if (!deepEquals(a[i], b[i])) return false;
     }
     return true;
   }
 
-  // 순수 객체 재귀 비교
-  const isObject = (val: unknown): val is Record<string, unknown> => {
-    return typeof val === "object" && val !== null && !Array.isArray(val);
-  };
+  const isObject = (val: unknown): val is Record<string, unknown> =>
+    typeof val === "object" && val !== null && !Array.isArray(val);
 
+  // 순수 객체 재귀 비교
   if (isObject(a) && isObject(b)) {
     const keysA = Object.keys(a);
+    const keysB = Object.keys(b);
+    if (keysA.length !== keysB.length) return false;
 
     for (const key of keysA) {
       if (!Object.prototype.hasOwnProperty.call(b, key)) return false;
