00bb0ea82e631d4d1196397c68d5d533a86bb958
minjaeleee <your-email@example.com>
2025-07-22 23:49:44 +0900

    feat: useShallowSelector 기능 구현 1. 이전 상태를 저장하고, shallowEquals를 사용해 상태가 변경되었는지 확인하는 로직

--- packages/lib/src/hooks/useShallowSelector.ts
+++ packages/lib/src/hooks/useShallowSelector.ts
@@ -1,9 +1,19 @@
-// import { useRef } from "react";
-// import { shallowEquals } from "../equals";
+import { useRef } from "react";
+import { shallowEquals } from "../equals";
 
 type Selector<T, S = T> = (state: T) => S;
 
 export const useShallowSelector = <T, S = T>(selector: Selector<T, S>) => {
-  // 이전 상태를 저장하고, shallowEquals를 사용하여 상태가 변경되었는지 확인하는 훅을 구현합니다.
-  return (state: T): S => selector(state);
+  const prevSelectedRef = useRef<S | null>(null);
+  return (state: T): S => {
+    // 이전 상태를 저장하고
+    const selected = selector(state);
+    // shallowEquals를 사용하여 상태가 변경되었는지 확인하는 훅을 구현합니다.
+    if (prevSelectedRef.current !== null && shallowEquals(prevSelectedRef.current, selected)) {
+      return prevSelectedRef.current;
+    }
+
+    prevSelectedRef.current = selected;
+    return selected;
+  };
 };
