0a915c508a59c136b7a13794ee762aaf206723a4
minjaeleee <your-email@example.com>
2025-07-23 00:03:16 +0900

    feat: useStore 기능 구현 1. 외부 상태 구독 방식 구현 2. shallow memoization을 결합한 방식으로 기능 구현

--- packages/lib/src/hooks/useStore.ts
+++ packages/lib/src/hooks/useStore.ts
@@ -1,5 +1,5 @@
 import type { createStore } from "../createStore";
-// import { useSyncExternalStore } from "react";
+import { useSyncExternalStore } from "react";
 import { useShallowSelector } from "./useShallowSelector";
 
 type Store<T> = ReturnType<typeof createStore<T>>;
@@ -7,7 +7,11 @@ type Store<T> = ReturnType<typeof createStore<T>>;
 const defaultSelector = <T, S = T>(state: T) => state as unknown as S;
 
 export const useStore = <T, S = T>(store: Store<T>, selector: (state: T) => S = defaultSelector<T, S>) => {
-  // useSyncExternalStore와 useShallowSelector를 사용해서 store의 상태를 구독하고 가져오는 훅을 구현해보세요.
+  // useSyncExternalStore와 useShallowSelector를 사용해서 store의 상태를 구독하고
   const shallowSelector = useShallowSelector(selector);
-  return shallowSelector(store.getState());
+
+  // 가져오는 훅을 구현해보세요.
+  const getSnapShot = () => shallowSelector(store.getState());
+
+  return useSyncExternalStore(store.subscribe, getSnapShot);
 };
